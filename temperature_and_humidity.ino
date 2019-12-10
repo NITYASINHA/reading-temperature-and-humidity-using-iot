5ac9adca9e7bbd2f58e55e7e46fa4abc815daf0c
NITYASINHA <43024320+NITYASINHA@users.noreply.github.com>
2019-12-10 11:36:13 +0530

    Add files via upload

---
+++ temperature_and_humidity.ino
@@ -0,0 +1,84 @@
+#include <ThingSpeak.h>
+
+#include <DHT.h>  // Including library for dht
+
+#include <ESP8266WiFi.h>
+ 
+String apiKey = "write your thinkspeak api";     
+const char *channelid = "write yur channel id";
+const char *ssid =  "write your ssid";     
+const char *pass =  "password";
+const char* server = "api.thingspeak.com";
+
+#define DHTPIN 0          
+ 
+DHT dht(DHTPIN, DHT11);
+
+WiFiClient client;
+ 
+void setup() 
+{
+       Serial.begin(115200);
+       delay(10);
+       dht.begin();
+ 
+       Serial.println("Connecting to ");
+       Serial.println(ssid);
+ 
+ 
+       WiFi.begin(ssid, pass);
+ 
+      while (WiFi.status() != WL_CONNECTED) 
+     {
+            delay(500);
+            Serial.print(".");
+     }
+      Serial.println("");
+      Serial.println("WiFi connected");
+ 
+}
+ 
+void loop() 
+{
+  
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      
+              if (isnan(h) || isnan(t)) 
+                 {
+                     Serial.println("Failed to read from DHT sensor!");
+                      return;
+                 }
+
+                         if (client.connect(server,80))  
+                      {  
+                            
+                             String postStr = apiKey;
+                             postStr +="&field1=";
+                             postStr += String(t);
+                             postStr +="&field2=";
+                             postStr += String(h);
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                             Serial.print("Temperature: ");
+                             Serial.print(t);
+                             Serial.print(" degrees Celcius, Humidity: ");
+                             Serial.print(h);
+                             Serial.println("%. Send to Thingspeak.");
+                        }
+          client.stop();
+ 
+          Serial.println("Waiting...");
+}
+  
+  
